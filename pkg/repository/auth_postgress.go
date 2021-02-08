6925137ebcbfe33892afdcd1fb2b15fb22ddfdcc
shoost <shoost666@bk.ru>
2021-02-09 00:05:15 +0300

    fix sign_up

--- pkg/repository/auth_postgress.go
+++ pkg/repository/auth_postgress.go
@@ -16,9 +16,9 @@ func NewAuthPostgres(db *sqlx.DB) *AuthPostgres {
 
 func (r *AuthPostgres) CreateUser(user rd_app.User) (int, error) {
 	var id int
-	query := fmt.Sprintf("INSERT INTO %s (name, email, password_hash) values ($1, $2, $3) RETURNING id", usersTable)
+	query := fmt.Sprintf("INSERT INTO %s (name, email, role, password_hash) values ($1, $2, $3, $4) RETURNING id", usersTable)
 
-	row := r.db.QueryRow(query, user.Name, user.Email, user.Password)
+	row := r.db.QueryRow(query, user.Name, user.Email, user.Role, user.Password)
 	if err := row.Scan(&id); err != nil {
 		return 0, err
 	}

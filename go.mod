8db0848ecd30ba4315ecbe1bd6dc886a99623190
shoost <shoost666@bk.ru>
2021-02-13 20:21:40 +0300

    remove CORS

--- go.mod
+++ go.mod
@@ -4,11 +4,9 @@ go 1.13
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
 	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
 	github.com/jmoiron/sqlx v1.3.1
-	github.com/joho/godotenv v1.3.0
 	github.com/lib/pq v1.9.0
 	github.com/sirupsen/logrus v1.7.0
 	github.com/spf13/viper v1.7.1
--- server.go
+++ server.go
@@ -2,8 +2,6 @@ package rd_app
 
 import (
 	"context"
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
 	"net/http"
 	"time"
 )
@@ -13,20 +11,6 @@ type Server struct {
 }
 
 func (s *Server) Run(port string, handler http.Handler) error {
-	router := gin.Default()
-
-	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"PUT", "PATCH"},
-		AllowHeaders:     []string{"Origin"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		AllowOriginFunc: func(origin string) bool {
-			return origin == "https://github.com"
-		},
-		MaxAge: 12 * time.Hour,
-	}))
-
 	s.httpServer = &http.Server{
 		Addr:           ":" + port,
 		Handler:        handler,

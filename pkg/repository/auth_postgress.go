bfdd9be1287bee0ff4fa8d0a59366fc692594224
shoost <shoost666@bk.ru>
2021-02-14 13:41:47 +0300

    fix GetUser

--- pkg/repository/auth_postgress.go
+++ pkg/repository/auth_postgress.go
@@ -26,10 +26,10 @@ func (r *AuthPostgres) CreateUser(user rd_app.User) (int, error) {
 	return id, nil
 }
 
-func (r *AuthPostgres) GetUser(username, password string) (rd_app.User, error) {
+func (r *AuthPostgres) GetUser(email, password string) (rd_app.User, error) {
 	var user rd_app.User
-	query := fmt.Sprintf("SELECT id FROM %s WHERE username=$1 AND password_hash=$2", usersTable)
-	err := r.db.Get(&user, query, username, password)
+	query := fmt.Sprintf("SELECT id FROM %s WHERE email=$1 AND password_hash=$2", usersTable)
+	err := r.db.Get(&user, query, email, password)
 
 	return user, err
 }

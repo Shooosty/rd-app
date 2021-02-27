b05156267c1a9c9e9226eb70b0f8eb2613122c98
shoost <shoost666@bk.ru>
2021-02-28 01:04:59 +0300

    fix user api

--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -3,6 +3,7 @@ CREATE TABLE users
     id            serial       not null unique,
     name          varchar(255) not null,
     role          varchar(255) not null,
+    phone         varchar(255) not null,
     email         varchar(255) not null unique,
     password_hash varchar(255) not null
 );

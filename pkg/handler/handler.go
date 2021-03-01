9d0fe1807ca32491208c0accf7a3d311b8036bbe
shoost <shoost666@bk.ru>
2021-03-01 20:55:15 +0300

    fix cors

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -6,6 +6,7 @@ import (
 	"github.com/shooosty/rd-app/pkg/service"
 	"github.com/swaggo/gin-swagger"
 	"github.com/swaggo/gin-swagger/swaggerFiles"
+	"time"
 )
 
 type Handler struct {
@@ -23,7 +24,12 @@ func (h *Handler) InitRoutes() *gin.Engine {
 		AllowOrigins:     []string{"https://rd-cabinet-7mds4.ondigitalocean.app", "http://localhost:3000", "localhost:3000"},
 		AllowMethods:     []string{"PUT, PATCH, GET, POST, OPTIONS"},
 		AllowHeaders:     []string{"Origin, Content-Length, X-Requested-With, Content-Type, Accept, Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
 	}))
 
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))

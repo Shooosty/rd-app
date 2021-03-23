bb5ea4ccfaf24137d1f764762c196bf8df96a794
shoost <shoost666@bk.ru>
2021-03-24 01:11:29 +0400

    add uuid generate fix

--- models/order.go
+++ models/order.go
@@ -2,21 +2,23 @@ package models
 
 import (
 	"errors"
+	"github.com/jinzhu/gorm"
+	uuid "github.com/satori/go.uuid"
 	"time"
 )
 
 type Order struct {
-	ID        string    `sql:"type:uuid;primary_key"`
+	ID        string    `sql:"type:uuid;primary_key;default:uuid_generate_v4()"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"update_at"`
 	UserId    string    `json:"user_id" db:"user_id" binding:"required"`
 	Name      string    `json:"name" binding:"required"`
 }
 
-//func (order *Order) BeforeCreate(scope *gorm.Scope) error {
-//	_ = scope.SetColumn("ID", uuid.NewV4().String())
-//	return nil
-//}
+func (order *Order) BeforeCreate(scope *gorm.Scope) error {
+	_ = scope.SetColumn("ID", uuid.NewV4().String())
+	return nil
+}
 
 type UpdateOrderInput struct {
 	Name *string `json:"name"`

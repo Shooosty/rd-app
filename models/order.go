09177e29200ca3334ab3cd0a2c4526f1777e9bf7
shoost <shoost666@bk.ru>
2021-04-05 16:44:46 +0300

    add orders struct

--- models/order.go
+++ models/order.go
@@ -8,11 +8,17 @@ import (
 )
 
 type Order struct {
-	ID        string    `sql:"type:uuid;primary_key;default:uuid_generate_v4()"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"update_at"`
-	UserId    string    `json:"user_id" db:"user_id" binding:"required"`
-	Name      string    `json:"name" binding:"required"`
+	ID          string    `sql:"type:uuid;primary_key;default:uuid_generate_v4()"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"update_at"`
+	UserId      string    `json:"user_id" db:"user_id" binding:"required"`
+	Address     string    `json:"address"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	Owner       string    `json:"owner"`
+	Contract    string    `json:"contract"`
+	Number      int       `sql:"serial;unique" json:"number"`
+	Datetime    string    `json:"datetime"`
 }
 
 func (order *Order) BeforeCreate(scope *gorm.Scope) error {

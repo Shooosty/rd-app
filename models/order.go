4f90c4baca8da24ee7154425c979497caaaf2c4b
shoost <shoost666@bk.ru>
2021-05-03 19:32:35 +0300

    add cred

--- models/order.go
+++ models/order.go
@@ -3,25 +3,26 @@ package models
 import (
 	"errors"
 	"github.com/jinzhu/gorm"
+	"github.com/lib/pq"
 	uuid "github.com/satori/go.uuid"
 	"time"
 )
 
 type Order struct {
-	ID             string    `sql:"type:uuid;primary_key;default:uuid_generate_v4()"`
-	CreatedAt      time.Time `json:"createdAt"`
-	UpdatedAt      time.Time `json:"updateAt"`
-	UserId         string    `json:"userId"`
-	Type           string    `json:"type"`
-	Address        string    `json:"address"`
-	Description    string    `json:"description"`
-	Status         string    `json:"status"`
-	Owner          string    `json:"owner"`
-	DesignerId     string    `json:"designerId"`
-	PhotographerId string    `json:"photographerId"`
-	Contract       string    `json:"contract"`
-	Datetime       string    `json:"datetime"`
-	PeopleIds      []string  `json:"people"`
+	ID             string         `sql:"type:uuid;primary_key;default:uuid_generate_v4()"`
+	CreatedAt      time.Time      `json:"createdAt"`
+	UpdatedAt      time.Time      `json:"updateAt"`
+	UserId         string         `json:"userId"`
+	Type           string         `json:"type"`
+	Address        string         `json:"address"`
+	Description    string         `json:"description"`
+	Status         string         `json:"status"`
+	Owner          string         `json:"owner"`
+	DesignerId     string         `json:"designerId"`
+	PhotographerId string         `json:"photographerId"`
+	Contract       string         `json:"contract"`
+	Datetime       string         `json:"datetime"`
+	PeopleIds      pq.StringArray `gorm:"type:string[]" json:"people"`
 }
 
 func (order *Order) BeforeCreate(scope *gorm.Scope) error {

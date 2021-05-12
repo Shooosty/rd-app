1960eaf621280a417dff1eecff7eb1682df8a013
shoost <shoost666@bk.ru>
2021-05-12 17:13:52 +0300

    add mail

--- models/order.go
+++ models/order.go
@@ -12,7 +12,7 @@ type Order struct {
 	CreatedAt      time.Time `json:"createdAt"`
 	UpdatedAt      time.Time `json:"updateAt"`
 	UserId         string    `json:"userId"`
-	Number         int       `json:"number"`
+	Number         int       `sql:"type:serial" json:"number"`
 	Address        string    `json:"address"`
 	Description    string    `json:"description"`
 	Owner          string    `json:"owner"`

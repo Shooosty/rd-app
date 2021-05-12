4fed34a658daafc7d33fb7f8ae04bda1126c96b2
shoost <shoost666@bk.ru>
2021-05-12 16:07:49 +0300

    add mail

--- models/order.go
+++ models/order.go
@@ -3,7 +3,6 @@ package models
 import (
 	"errors"
 	"github.com/jinzhu/gorm"
-	"github.com/lib/pq"
 	uuid "github.com/satori/go.uuid"
 	"time"
 )
@@ -13,7 +12,7 @@ type Order struct {
 	CreatedAt      time.Time `json:"createdAt"`
 	UpdatedAt      time.Time `json:"updateAt"`
 	UserId         string    `json:"userId"`
-	Type           string    `json:"type"`
+	Number         int64     `json:"number"`
 	Address        string    `json:"address"`
 	Description    string    `json:"description"`
 	Owner          string    `json:"owner"`
@@ -30,16 +29,15 @@ func (order *Order) BeforeCreate(scope *gorm.Scope) error {
 }
 
 type UpdateOrderInput struct {
-	Address        *string         `json:"address"`
-	Status         *string         `json:"status"`
-	Owner          *string         `json:"owner"`
-	UserId         *string         `json:"userId"`
-	DesignerId     *string         `json:"designerId"`
-	PhotographerId *string         `json:"photographerId"`
-	PeopleIds      *pq.StringArray `sql:"type:text[]" json:"people"`
-	Contract       *string         `json:"contract"`
-	Datetime       *string         `json:"datetime"`
-	Description    *string         `json:"description"`
+	Address        *string `json:"address"`
+	Status         *string `json:"status"`
+	Owner          *string `json:"owner"`
+	UserId         *string `json:"userId"`
+	DesignerId     *string `json:"designerId"`
+	PhotographerId *string `json:"photographerId"`
+	Contract       *string `json:"contract"`
+	Datetime       *string `json:"datetime"`
+	Description    *string `json:"description"`
 }
 
 func (i UpdateOrderInput) Validate() error {

938f26c961de215c238f9969b3d0da0a304f7ede
shoost <shoost666@bk.ru>
2021-05-16 14:31:49 +0300

    fixes and features

--- models/person.go
+++ models/person.go
@@ -16,7 +16,7 @@ type Person struct {
 	Name        string    `json:"name"`
 	Surname     string    `json:"surname"`
 	Type        string    `json:"type"`
-	Photos      []Photos  `sql:"type:jsonb" json:"photos"`
+	Photos      []Photos  `json:"photos"`
 	MiddleName  string    `json:"middleName"`
 }
 

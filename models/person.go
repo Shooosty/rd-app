56eb66b0f7393c8ccde670b44675b4d9955c9c96
shoost <shoost666@bk.ru>
2021-05-16 13:26:32 +0300

    fixes and features

--- models/person.go
+++ models/person.go
@@ -9,21 +9,28 @@ import (
 )
 
 type Person struct {
-	ID          string          `sql:"type:uuid;primary_key;default:uuid_generate_v4()"`
-	OrderId     string          `json:"orderId"`
-	CreatedAt   time.Time       `json:"createdAt"`
-	UpdatedAt   time.Time       `json:"updateAt"`
-	Description string          `json:"description"`
-	Name        string          `json:"name"`
-	Surname     string          `json:"surname"`
-	Type        string          `json:"type"`
-	Photos      *pq.StringArray `sql:"type:text[]" json:"photos"`
-	MiddleName  string          `json:"middleName"`
+	ID          string    `sql:"type:uuid;primary_key;default:uuid_generate_v4()"`
+	OrderId     string    `json:"orderId"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updateAt"`
+	Description string    `json:"description"`
+	Name        string    `json:"name"`
+	Surname     string    `json:"surname"`
+	Type        string    `json:"type"`
+	Photos      Photos    `sql:"type:text[]" json:"photos"`
+	MiddleName  string    `json:"middleName"`
 }
 
 func (person *Person) BeforeCreate(scope *gorm.Scope) error {
 	_ = scope.SetColumn("ID", uuid.NewV4().String())
 	return nil
+}
+
+type Photos struct {
+	Name string `json:"name"`
+	Size string `json:"size"`
+	Type string `json:"type"`
+	Url  string `json:"url"`
 }
 
 type UpdatePersonInput struct {

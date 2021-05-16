30a2f19ca997b7aac179c42f86b90964c8a78c9c
shoost <shoost666@bk.ru>
2021-05-16 14:52:37 +0300

    fixes and features

--- models/person.go
+++ models/person.go
@@ -3,21 +3,22 @@ package models
 import (
 	"errors"
 	"github.com/jinzhu/gorm"
+	"github.com/jinzhu/gorm/dialects/postgres"
 	uuid "github.com/satori/go.uuid"
 	"time"
 )
 
 type Person struct {
-	ID          string    `sql:"type:uuid;primary_key;default:uuid_generate_v4()"`
-	OrderId     string    `json:"orderId"`
-	CreatedAt   time.Time `json:"createdAt"`
-	UpdatedAt   time.Time `json:"updateAt"`
-	Description string    `json:"description"`
-	Name        string    `json:"name"`
-	Surname     string    `json:"surname"`
-	Type        string    `json:"type"`
-	Photos      []Photos  `sql:"type:jsonb[]" json:"photos"`
-	MiddleName  string    `json:"middleName"`
+	ID          string          `sql:"type:uuid;primary_key;default:uuid_generate_v4()"`
+	OrderId     string          `json:"orderId"`
+	CreatedAt   time.Time       `json:"createdAt"`
+	UpdatedAt   time.Time       `json:"updateAt"`
+	Description string          `json:"description"`
+	Name        string          `json:"name"`
+	Surname     string          `json:"surname"`
+	Type        string          `json:"type"`
+	Photos      *postgres.Jsonb `sql:"type:jsonb[]" json:"photos"`
+	MiddleName  string          `json:"middleName"`
 }
 
 func (person *Person) BeforeCreate(scope *gorm.Scope) error {
@@ -33,12 +34,12 @@ type Photos struct {
 }
 
 type UpdatePersonInput struct {
-	Name        *string `json:"name"`
-	Surname     *string `json:"surname"`
-	MiddleName  *string `json:"middleName"`
-	Type        *string `json:"type"`
-	Description *string `json:"description"`
-	Photos      *Photos `json:"photos"`
+	Name        *string         `json:"name"`
+	Surname     *string         `json:"surname"`
+	MiddleName  *string         `json:"middleName"`
+	Type        *string         `json:"type"`
+	Description *string         `json:"description"`
+	Photos      *postgres.Jsonb `json:"photos"`
 }
 
 func (i UpdatePersonInput) Validate() error {

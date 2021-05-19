16cd89d15e9f216dbcb24a5e5220467925c65aa2
shoost <shoost666@bk.ru>
2021-05-19 17:45:32 +0300

    fixes and features

--- models/photo.go
+++ models/photo.go
@@ -6,11 +6,12 @@ import (
 )
 
 type Photo struct {
-	ID   string `sql:"type:uuid;primary_key;default:uuid_generate_v4()"`
-	Name string `json:"name"`
-	Url  string `json:"url"`
-	Size int64  `json:"surname"`
-	Type string `json:"type"`
+	ID       string `sql:"type:uuid;primary_key;default:uuid_generate_v4()"`
+	PersonID string `json:"personId"`
+	Name     string `json:"name"`
+	Url      string `json:"url"`
+	Size     int64  `json:"surname"`
+	Type     string `json:"type"`
 }
 
 func (person *Photo) BeforeCreate(scope *gorm.Scope) error {

2427ff89b6240ad0f8df11458bca9543e69e7e15
shoost <shoost666@bk.ru>
2021-05-23 17:39:52 +0300

    fixes and features

--- pkg/handler/middleware.go
+++ pkg/handler/middleware.go
@@ -20,7 +20,7 @@ func (h *Handler) userIdentity(c *gin.Context) {
 	}
 
 	headerParts := strings.Split(header, " ")
-	if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+	if len(headerParts) != 2 {
 		newErrorResponse(c, http.StatusUnauthorized, "invalid auth header")
 		return
 	}

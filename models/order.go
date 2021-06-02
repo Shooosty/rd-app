31ecaddfe6d390f8987e70956203072a66b94d51
shoost <shoost666@bk.ru>
2021-06-02 21:02:28 +0300

    fixes and features

--- models/order.go
+++ models/order.go
@@ -9,23 +9,23 @@ import (
 )
 
 type Order struct {
-	ID             string         `sql:"type:uuid;primary_key;default:uuid_generate_v4()"`
-	Number         string         `json:"number"`
-	Sections       pq.StringArray `sql:"type: text[]" json:"sections"`
-	CreatedAt      time.Time      `json:"createdAt"`
-	UpdatedAt      time.Time      `json:"updateAt"`
-	UserId         string         `json:"userId"`
-	Address        string         `json:"address"`
-	Description    string         `json:"description"`
-	Owner          string         `json:"owner"`
-	DesignerId     string         `json:"designerId"`
-	PhotographerId string         `json:"photographerId"`
-	ManagerId      string         `json:"managerId"`
-	Status         string         `json:"status"`
-	Contract       string         `json:"contract"`
-	Attachment     string         `json:"attachment"`
-	Layout         string         `json:"layout"`
-	Datetime       string         `json:"datetime"`
+	ID                 string         `sql:"type:uuid;primary_key;default:uuid_generate_v4()"`
+	Number             string         `json:"number"`
+	Sections           pq.StringArray `sql:"type: text[]" json:"sections"`
+	CreatedAt          time.Time      `json:"createdAt"`
+	UpdatedAt          time.Time      `json:"updateAt"`
+	UserId             string         `json:"userId"`
+	Address            string         `json:"address"`
+	Description        string         `json:"description"`
+	Owner              string         `json:"owner"`
+	DesignerId         string         `json:"designerId"`
+	PhotographerId     string         `json:"photographerId"`
+	ManagerId          string         `json:"managerId"`
+	Status             string         `json:"status"`
+	Contract           string         `json:"contract"`
+	AttachmentContract string         `json:"attachmentContract"`
+	Layout             string         `json:"layout"`
+	Datetime           string         `json:"datetime"`
 }
 
 func (order *Order) BeforeCreate(scope *gorm.Scope) error {
@@ -34,25 +34,25 @@ func (order *Order) BeforeCreate(scope *gorm.Scope) error {
 }
 
 type UpdateOrderInput struct {
-	Status         *string `json:"status"`
-	Address        *string `json:"address"`
-	Number         *string `json:"number"`
-	Owner          *string `json:"owner"`
-	UserId         *string `json:"userId"`
-	DesignerId     *string `json:"designerId"`
-	ManagerId      *string `json:"managerId"`
-	PhotographerId *string `json:"photographerId"`
-	Contract       *string `json:"contract"`
-	Attachment     *string `json:"attachment"`
-	Layout         *string `json:"layout"`
-	Datetime       *string `json:"datetime"`
-	Description    *string `json:"description"`
+	Status             *string `json:"status"`
+	Address            *string `json:"address"`
+	Number             *string `json:"number"`
+	Owner              *string `json:"owner"`
+	UserId             *string `json:"userId"`
+	DesignerId         *string `json:"designerId"`
+	ManagerId          *string `json:"managerId"`
+	PhotographerId     *string `json:"photographerId"`
+	Contract           *string `json:"contract"`
+	AttachmentContract *string `json:"attachmentContract"`
+	Layout             *string `json:"layout"`
+	Datetime           *string `json:"datetime"`
+	Description        *string `json:"description"`
 }
 
 func (i UpdateOrderInput) Validate() error {
 	if i.Address == nil && i.Status == nil &&
 		i.Owner == nil && i.UserId == nil && i.DesignerId == nil &&
-		i.ManagerId == nil && i.Layout == nil && i.Attachment == nil &&
+		i.ManagerId == nil && i.Layout == nil && i.AttachmentContract == nil &&
 		i.PhotographerId == nil && i.Number == nil &&
 		i.Description == nil && i.Datetime == nil {
 		return errors.New("update structure has no values")

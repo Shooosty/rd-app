5d362936e4d3075727d0e9068ef1d07781928dbe
shoost <shoost666@bk.ru>
2021-06-02 16:17:52 +0300

    fixes and features

--- models/order.go
+++ models/order.go
@@ -3,24 +3,27 @@ package models
 import (
 	"errors"
 	"github.com/jinzhu/gorm"
+	"github.com/lib/pq"
 	uuid "github.com/satori/go.uuid"
 	"time"
 )
 
 type Order struct {
-	ID             string    `sql:"type:uuid;primary_key;default:uuid_generate_v4()"`
-	Number         string    `json:"number"`
-	CreatedAt      time.Time `json:"createdAt"`
-	UpdatedAt      time.Time `json:"updateAt"`
-	UserId         string    `json:"userId"`
-	Address        string    `json:"address"`
-	Description    string    `json:"description"`
-	Owner          string    `json:"owner"`
-	DesignerId     string    `json:"designerId"`
-	PhotographerId string    `json:"photographerId"`
-	Status         string    `json:"status"`
-	Contract       string    `json:"contract"`
-	Datetime       string    `json:"datetime"`
+	ID             string         `sql:"type:uuid;primary_key;default:uuid_generate_v4()"`
+	Number         string         `json:"number"`
+	Sections       pq.StringArray `json:"sections"`
+	CreatedAt      time.Time      `json:"createdAt"`
+	UpdatedAt      time.Time      `json:"updateAt"`
+	UserId         string         `json:"userId"`
+	Address        string         `json:"address"`
+	Description    string         `json:"description"`
+	Owner          string         `json:"owner"`
+	DesignerId     string         `json:"designerId"`
+	PhotographerId string         `json:"photographerId"`
+	ManagerId      string         `json:"managerId"`
+	Status         string         `json:"status"`
+	Contract       string         `json:"contract"`
+	Datetime       string         `json:"datetime"`
 }
 
 func (order *Order) BeforeCreate(scope *gorm.Scope) error {
@@ -35,6 +38,7 @@ type UpdateOrderInput struct {
 	Owner          *string `json:"owner"`
 	UserId         *string `json:"userId"`
 	DesignerId     *string `json:"designerId"`
+	ManagerId      *string `json:"managerId"`
 	PhotographerId *string `json:"photographerId"`
 	Contract       *string `json:"contract"`
 	Datetime       *string `json:"datetime"`
@@ -44,6 +48,7 @@ type UpdateOrderInput struct {
 func (i UpdateOrderInput) Validate() error {
 	if i.Address == nil && i.Status == nil &&
 		i.Owner == nil && i.UserId == nil && i.DesignerId == nil &&
+		i.ManagerId == nil &&
 		i.PhotographerId == nil && i.Number == nil &&
 		i.Description == nil && i.Datetime == nil {
 		return errors.New("update structure has no values")

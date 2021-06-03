34421d055eae635829a86afd9910296497e1c778
shoost <shoost666@bk.ru>
2021-06-03 21:09:28 +0300

    fixes and features

--- pkg/handler/bitrix.go
+++ pkg/handler/bitrix.go
@@ -1,12 +1,12 @@
 package handler
 
 import (
-	"github.com/gin-gonic/gin"
+	"io"
 	"log"
 	"net/http"
 )
 
-func (h *Handler) getAllBitrixOrders(c *gin.Context) {
+func (h *Handler) getAllBitrixOrders() io.ReadCloser {
 	resp, err := http.Get("https://rosfotoproekt.bitrix24.ru/rest/3872/l00jxlvjy0aamuom/crm.deal.list.json")
 	if err != nil {
 		log.Fatalln(err)
@@ -14,7 +14,5 @@ func (h *Handler) getAllBitrixOrders(c *gin.Context) {
 
 	body := resp.Body
 
-	c.JSON(http.StatusOK, map[string]interface{}{
-		"data": body,
-	})
+	return body
 }

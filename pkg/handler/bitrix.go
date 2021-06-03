7e2e35005c939cc6e51849e32580b018d56e350c
shoost <shoost666@bk.ru>
2021-06-03 22:37:48 +0300

    fixes and features

--- pkg/handler/bitrix.go
+++ pkg/handler/bitrix.go
@@ -1,23 +1,34 @@
 package handler
 
 import (
+	"encoding/json"
 	"github.com/gin-gonic/gin"
-	"log"
 	"net/http"
+	"time"
 )
 
-type order struct {
-	ID    int
-	TITLE string
+type Orders struct {
+	ID int
+}
+
+var myClient = &http.Client{Timeout: 10 * time.Second}
+
+func getJson(url string, target interface{}) error {
+	r, err := myClient.Get(url)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	return json.NewDecoder(r.Body).Decode(target)
 }
 
 func (h *Handler) getAllBitrixOrders(c *gin.Context) {
-	resp, err := http.Get("https://rosfotoproekt.bitrix24.ru/rest/3872/l00jxlvjy0aamuom/crm.deal.list.json")
-	if err != nil {
-		log.Fatalln(err)
-	}
+
+	result := Orders{}
+	_ = getJson("https://rosfotoproekt.bitrix24.ru/rest/3872/l00jxlvjy0aamuom/crm.deal.list.json", &result)
 
 	c.JSON(http.StatusOK, map[string]interface{}{
-		"data": resp.Body,
+		"data": result,
 	})
 }

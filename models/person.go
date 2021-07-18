635d3f206c5f5c25b8e41e673d93cd72782d1d23
shoost <shoost666@bk.ru>
2021-07-18 21:15:02 +0400

    fixes and features

--- models/person.go
+++ models/person.go
@@ -3,24 +3,25 @@ package models
 import (
 	"errors"
 	"github.com/jinzhu/gorm"
+	"github.com/lib/pq"
 	uuid "github.com/satori/go.uuid"
 	"time"
 )
 
 type Person struct {
-	ID          string    `sql:"type:uuid;primary_key;default:uuid_generate_v4()"`
-	OrderId     string    `json:"orderId"`
-	CreatedAt   time.Time `json:"createdAt"`
-	UpdatedAt   time.Time `json:"updateAt"`
-	Description string    `json:"description"`
-	Name        string    `json:"name"`
-	WillBuy     string    `json:"willBuy"`
-	Tz          string    `json:"tz"`
-	Role        string    `json:"role"`
-	Surname     string    `json:"surname"`
-	Type        string    `json:"type"`
-	MiddleName  string    `json:"middleName"`
-	PhotosCount int       `json:"photosCount"`
+	ID          string         `sql:"type:uuid;primary_key;default:uuid_generate_v4()"`
+	OrderId     string         `json:"orderId"`
+	CreatedAt   time.Time      `json:"createdAt"`
+	UpdatedAt   time.Time      `json:"updateAt"`
+	Description string         `json:"description"`
+	Name        string         `json:"name"`
+	WillBuy     string         `json:"willBuy"`
+	Tz          pq.StringArray `sql:"type: text[]" json:"tz"`
+	Role        string         `json:"role"`
+	Surname     string         `json:"surname"`
+	Type        string         `json:"type"`
+	MiddleName  string         `json:"middleName"`
+	PhotosCount int            `json:"photosCount"`
 }
 
 func (person *Person) BeforeCreate(scope *gorm.Scope) error {
@@ -29,15 +30,15 @@ func (person *Person) BeforeCreate(scope *gorm.Scope) error {
 }
 
 type UpdatePersonInput struct {
-	Name        *string `json:"name"`
-	Surname     *string `json:"surname"`
-	MiddleName  *string `json:"middleName"`
-	WillBuy     *string `json:"willBuy"`
-	Tz          *string `json:"tz"`
-	Role        *string `json:"role"`
-	Type        *string `json:"type"`
-	Description *string `json:"description"`
-	PhotosCount *int    `json:"photosCount"`
+	Name        *string         `json:"name"`
+	Surname     *string         `json:"surname"`
+	MiddleName  *string         `json:"middleName"`
+	WillBuy     *string         `json:"willBuy"`
+	Tz          *pq.StringArray `json:"tz"`
+	Role        *string         `json:"role"`
+	Type        *string         `json:"type"`
+	Description *string         `json:"description"`
+	PhotosCount *int            `json:"photosCount"`
 }
 
 func (i UpdatePersonInput) Validate() error {

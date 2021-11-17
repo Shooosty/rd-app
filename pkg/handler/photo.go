8cb8b2360630d20aaf2e5bbc635fe359cc690776
shoosty <Raion199126>
2021-11-17 18:33:36 +0300

    features && fixes

--- pkg/handler/photo.go
+++ pkg/handler/photo.go
@@ -96,12 +96,12 @@ func (h *Handler) createPhoto(c *gin.Context) {
 	}
 	defer file.Close()
 
-	keyName, originalName, size, err := UploadPhotoToS3(file, fileName, header)
-
-	if err != nil {
-		newErrorResponse(c, http.StatusBadRequest, "Could not upload file")
-		return
-	}
+	//keyName, originalName, size, err := UploadPhotoToS3(file, fileName, header)
+	//
+	//if err != nil {
+	//	newErrorResponse(c, http.StatusBadRequest, "Could not upload file")
+	//	return
+	//}
 
 	keyNameResize, err := UploadResizedPhotoToS3(file, fileName, header)
 
@@ -110,14 +110,14 @@ func (h *Handler) createPhoto(c *gin.Context) {
 		return
 	}
 
-	url := "https://rhinodesign.s3.eu-west-3.amazonaws.com/" + keyName
+	//url := "https://rhinodesign.s3.eu-west-3.amazonaws.com/" + keyName
 	urlResize := "https://rhinodesign.s3.eu-west-3.amazonaws.com/" + keyNameResize
 
-	input.Name = originalName
-	input.NameS3 = keyName
-	input.Url = url
+	//input.Name = originalName
+	//input.NameS3 = keyName
+	//input.Url = url
 	input.UrlResize = urlResize
-	input.Size = size / 1024
+	//input.Size = size / 1024
 	input.PersonId = personId
 	input.OrderId = orderId
 	input.Type = "image/*"
